379454f62a37704d0b05fc36f0e9354e1be2add5
stanochka <krozzochka@gmail.com>
2021-06-05 23:31:06 +0300

    Make basic functions for operations

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const add = (a, b) => {
+  return a + b;
+};
 
+const substract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return (b !== 0 ? a / b : 'ERROR';
+};
+
+const operate = (operator, a, b) => {
+  if (operator == 'add') {
+    return add(a, b);
+  } else if (operator == 'substract') {
+    return substract(a, b);
+  } else if (operator == 'multiply') {
+    return multiply(a, b);
+  } else if (operator == 'divide') {
+    return divide(a, b);
+  }; 
+};
