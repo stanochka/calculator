aad86c6ee8636502f2ba8c08bd657794dfaed491
stanochka <krozzochka@gmail.com>
2021-06-06 00:17:20 +0300

    Make working upon button click

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const multiply = (a, b) => {
 };
 
 const divide = (a, b) => {
-  return (b !== 0 ? a / b : 'ERROR';
+  return (b !== 0 ? a / b : 'ERROR');
 };
 
 const operate = (operator, a, b) => {
@@ -23,5 +23,35 @@ const operate = (operator, a, b) => {
     return multiply(a, b);
   } else if (operator == 'divide') {
     return divide(a, b);
-  }; 
+  };
 };
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+var num = [];
+var arr = [];
+var operator;
+var result;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (!isNaN(button.id)) {
+      num.push(button.id);
+      display.textContent = num.join('');
+    } else if (button.id !== 'clear' && button.id !== 'equals') {
+      arr.push(parseInt(num.join('')));
+      num.length = 0;
+      operator = button.id;
+    } else if (button.id === 'clear') {
+      display.textContent = '0';
+      num.length = 0;
+      arr.length = 0;
+    } else if (button.id === 'equals') {
+      arr.push(parseInt(num.join('')));
+      num.length = 0;
+      result = operate(operator, ...arr);
+      display.textContent = result;
+      arr = [result];
+    }
+  })
+})
